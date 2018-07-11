323006230bd747dfab67baaedbbd25c2ec895ee5
steh42 <41127607+steh42@users.noreply.github.com>
2018-07-11 16:58:43 -0300

    add answer to activity 1006

---
+++ Activiy_1006/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double A,B,C;
+    double MEDIA;
+
+    scanf("%lf" ,&A);
+    scanf("%lf" ,&B);
+    scanf("%lf" , &C);
+
+    MEDIA = (2*A + 3*B + 5*C) / 10;
+
+    printf( "MEDIA = %.1f\n" , MEDIA);
+	
+    return 0;
+}
